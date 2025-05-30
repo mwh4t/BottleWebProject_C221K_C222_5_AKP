d270c08bc8ff961cb341dcf9cd83110328a1f747
mwh4t <korzunov.matvey@yandex.ru>
2025-05-30 12:44:59 +0300

    app file returned

---
+++ app.py
@@ -0,0 +1,24 @@
+import bottle
+import os
+import sys
+import routes
+
+
+if '--debug' in sys.argv[1:] or 'SERVER_DEBUG' in os.environ:
+    bottle.debug(True)
+
+def wsgi_app():
+    return bottle.default_app()
+
+if __name__ == '__main__':
+    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+    STATIC_ROOT = (os.path.join(PROJECT_ROOT, 'static')
+                   .replace('\\', '/'))
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    PORT = os.environ.get('SERVER_PORT', 5555)
+
+    @bottle.route('/static/<filepath:path>')
+    def server_static(filepath):
+        return bottle.static_file(filepath, root=STATIC_ROOT)
+
+    bottle.run(server='wsgiref', host=HOST, port=PORT)
